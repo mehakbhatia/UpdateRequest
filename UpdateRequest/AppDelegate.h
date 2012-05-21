5163db191b74d417355d0d80f1c30a9d57945231
Mehak <mehak.b@DV-MAC-PIYUSH-A.local>
2012-05-21 03:50:32 -0700

    Initial Commit

---
+++ UpdateRequest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UpdateRequest
+//
+//  Created by Mehak on 5/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UpdateRequestTests/UpdateRequestTests.m
@@ -0,0 +1,32 @@
+//
+//  UpdateRequestTests.m
+//  UpdateRequestTests
+//
+//  Created by Mehak on 5/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "UpdateRequestTests.h"
+
+@implementation UpdateRequestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in UpdateRequestTests");
+}
+
+@end
